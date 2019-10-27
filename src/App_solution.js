071c3b1e61284ecf8c220a5849783b6f6b335fd7
Schubi <32953964+Schuubii@users.noreply.github.com>
2019-10-27 21:58:50 +0100

    feat(added): CSS styling und inlinestyling, Lösung für Task hinzugefügt

--- src/App_solution.js
+++ src/App_solution.js
@@ -9,6 +9,8 @@ function App() {
     person: [{ name: "Max", age: 23 }, { name: "Daniel", age: 27 }]
   });
 
+  const [showPerson, setShowPerson] = useState(false)
+
   const style = {
     backgroundColor: 'white',
     font: 'inherit',
@@ -17,10 +19,11 @@ function App() {
     cursor: 'pointer'
   }
 
-  return (
-    
-
-    <div className="App">
+  /* JavaScript lösung
+  let people = null;
+  if(showPerson){
+    people = (
+      <div> 
       <Person name={"Alan"} age={27} />
 
       <Person
@@ -50,16 +53,56 @@ function App() {
         My Hobby: Playing WoW - For the Horde!!^1
       </Person>
 
+      </div>
+    )
+  }
+  */
+
+  return (
+    <div className="App">
       <button
         style = {style}
-        onClick={() =>
+        onClick={
+          //() =>setPeople({person: [{ name: "Sascha", age: 29 }, { ...people.person[1] }]})
+          () => setShowPerson(!showPerson)
+        }
+        >
+        Toggle Person
+      </button>
+
+      {/*{people} -- JavaScript Lösung*/}  
+
+      { showPerson ? <div> 
+      <Person name={"Alan"} age={27} />
+
+      <Person
+        name={people.person[0].name}
+        age={people.person[0].age}
+        //React hook - Ändert namen auf der Seite mit jedem Keystroke
+        changed={event =>
           setPeople({
-            person: [{ name: "Sascha", age: 29 }, { ...people.person[1] }]
+            person: [
+              { name: event.target.value, age: 23 },
+              { ...people.person[1] }
+            ]
           })
         }
+      />
+
+      <Person
+        //Hook - ändert namen wenn du auf den Paragraph klickst
+        click={() =>
+          setPeople({
+            person: [{ ...people.person[0] }, { name: "Moritz", age: 102 }]
+          })
+        }
+        name={people.person[1].name}
+        age={people.person[1].age}
       >
-        Switch it!
-      </button>
+        My Hobby: Playing WoW - For the Horde!!^1
+      </Person>
+
+      </div> : null}
     </div>
   );
 }
